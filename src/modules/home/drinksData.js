f066c8d4419069cc815b46f84ee04b4b09b4354f
jonathan-pe <jonathanqpe@gmail.com>
2021-10-11 16:21:11 -0700

    feat: simplify UI and start adding drinks data to display

--- src/modules/home/drinksData.js
+++ src/modules/home/drinksData.js
@@ -1,3 +1,28 @@
-const drinksData = {}
+const drinksData = [
+  {
+    place: 'New York',
+    country: 'United States of America',
+    name: 'Manhattan',
+    drinkImg: null,
+    funFact: null,
+    ingredients: {},
+  },
+  {
+    place: 'Port of Spain',
+    country: 'Trinidad & Tobago',
+    name: 'Port of Spain',
+    drinkImg: null,
+    funFact: null,
+    ingredients: {},
+  },
+  {
+    place: 'Mexico City',
+    country: 'Mexico',
+    name: 'Mexico City Cocktail',
+    drinkImg: null,
+    funFact: null,
+    ingredients: {},
+  },
+]
 
 export default drinksData
--- src/modules/home/home.jsx
+++ src/modules/home/home.jsx
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 import ct from 'countries-and-timezones'
 import { useEffect, useState } from 'react'
 import moment from 'moment-timezone'
-import Clock from 'react-clock'
 import 'react-clock/dist/Clock.css'
+import _ from 'lodash'
+import drinksData from './drinksData'
 
 const HomeBox = styled(Box)`
   display: flex;
@@ -20,13 +21,27 @@ const DataBox = styled(Box)`
   align-items: center;
 `
 
+/**
+ * TODO: moment.js (and perhaps moment-timezone) has gone into legacy mode
+ * so we might have to redo calculations to determine when it's 5pm in certain
+ * timezones. I've already pre-emptively added /lib/isDST.js but might have to
+ * eventually transition into calculating local times using UTC offsets
+ *
+ * We still need to find better ways to really programmatically drill down into
+ * specific countries/cities that exist within these time zones. Best way is probably
+ * just to hardcode them like we're doing now.
+ *
+ */
+
 const Home = () => {
   const [currentTime, setCurrentTime] = useState(new Date())
-  const [fiveTime, setFiveTime] = useState(new Date())
   const [placesAt5, setPlacesAt5] = useState([])
+  const [placeOfChoice, setPlaceOfChoice] = useState()
+  const [drinkOfChoice, setDrinkOfChoice] = useState()
 
   const updateZones = () => {
     const allTimeZoneNames = moment.tz.names()
+    console.log(ct.getTimezonesForCountry('US'))
 
     const places = []
 
@@ -36,31 +51,28 @@ const Home = () => {
         const zoneName = tz.tz()
 
         const nameArray = zoneName.split('/')
+        const country = ct.getCountryForTimezone(zoneName)
         if (nameArray.length > 1 && nameArray[0] !== 'Etc') {
           places.push({
             zoneName,
-            place: nameArray[1].replace('_', ' '),
-            country: ct.getCountryForTimezone(zoneName).name,
+            place: nameArray[1].replaceAll('_', ' '),
+            country: country && country.name,
           })
         }
       }
     })
 
     setPlacesAt5(places)
-    console.log(places)
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
       const currTime = new Date()
-      const tempFiveTime = new Date()
 
-      if (tempFiveTime.getHours !== 17) {
-        tempFiveTime.setHours(17)
+      if (currTime.getMinutes() === 0 && currTime.getSeconds() === 0) {
         updateZones()
       }
       setCurrentTime(currTime)
-      setFiveTime(tempFiveTime)
     }, 900) // wasn't always updating the time every second when set at 1000
 
     return () => {
@@ -71,15 +83,43 @@ const Home = () => {
   useEffect(() => {
     updateZones()
   }, [])
+
+  useEffect(() => {
+    findDrink(placesAt5)
+  }, [placesAt5])
+
+  const findDrink = () => {
+    let place = placesAt5[_.random(0, placesAt5.length - 1)]
+    let drink = null
+
+    placesAt5.forEach(p => {
+      if (drinksData.some(drink => drink.place === p.place)) {
+        place = p
+        drink = drinksData.find(drink => drink.place === p.place)
+      }
+    })
+
+    setDrinkOfChoice(drink)
+    setPlaceOfChoice(place)
+  }
+
   return (
     <HomeBox>
-      <Typography variant='h2'>It's 5 O'Clock Somewhere</Typography>
-      <Typography>{currentTime.toLocaleString()}</Typography>
-      <Clock value={currentTime} />
-      <Typography>{fiveTime.toLocaleString()}</Typography>
-      <Clock value={fiveTime} />
+      <Typography variant='h2' sx={{ mb: 5 }}>
+        It's 5 O'Clock Somewhere
+      </Typography>
+      <Typography component='div' variant='h4' sx={{ mb: 5, color: 'text.secondary' }}>
+        It's currently{' '}
+        <Typography component='span' variant='h4' sx={{ ml: 1, mr: 1, color: 'secondary.main', fontWeight: '600' }}>
+          {currentTime.toLocaleString()}
+        </Typography>{' '}
+        in{' '}
+        <Typography component='span' variant='h4' sx={{ ml: 1, mr: 1, fontWeight: '600', color: 'text.primary' }}>
+          {placeOfChoice.place}, {placeOfChoice.country}
+        </Typography>
+      </Typography>
       <DataBox>
-        <List>
+        {/* <List>
           {placesAt5.map(place => {
             return (
               <ListItem key={place.zoneName}>
@@ -89,8 +129,17 @@ const Home = () => {
               </ListItem>
             )
           })}
-        </List>
-        <Typography>Drink photo here</Typography>
+        </List> */}
+        {placeOfChoice && (
+          <Box>
+            <Typography></Typography>
+          </Box>
+        )}
+        {drinkOfChoice && (
+          <Box sx={{ ml: 10 }}>
+            <Typography>{drinkOfChoice.name}</Typography>
+          </Box>
+        )}
       </DataBox>
     </HomeBox>
   )
